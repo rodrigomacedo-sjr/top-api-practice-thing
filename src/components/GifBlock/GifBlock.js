c1a754cf7cfcf92ad2bbd66d9198f6bc019d6085
Rodrigo Macedo Silva Junior <rodrigo.junior@platformscience.com>
2026-04-22 22:31:18 -0300

    feat: gif loading on screen
    Signed-off-by: Rodrigo Macedo Silva Junior <rodrigo.junior@platformscience.com>

--- src/components/GifBlock/GifBlock.js
+++ src/components/GifBlock/GifBlock.js
@@ -1,5 +1,22 @@
-const GifBlock = function () {
-  return document.createElement("div");
-};
+const GifBlock = (function () {
+  const gif = document.createElement("img");
+
+  const reload = () => {
+    fetch(
+      "https://api.giphy.com/v1/gifs/translate?api_key=65FESTa9dwAbZhFGa4X5DHYBLi9c6xy3&s=cats",
+    )
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (response) {
+        gif.src = response.data.images.original.url;
+      });
+  };
+
+  return {
+    element: gif,
+    reload,
+  };
+})();
 
 export default GifBlock;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import GifBlock from "./components/GifBlock/GifBlock.js";
 import ThemeToggle from "./components/ThemeToggle/ThemeToggle.js";
 import ThemeHandler from "./theme.js";
 
-
 const content = document.getElementById("content");
 
-content.appendChild(GifBlock());
+content.appendChild(GifBlock.element);
+GifBlock.reload();
 
 content.appendChild(ThemeToggle());
 
